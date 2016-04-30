8c4365abcd52cdd73d8c2c40426499c1ef99bb2c
CodeDotJS <rushforlinux@gmail.com>
2016-05-01 01:40:13 +0530

    final

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 'use strict';
-module.exports = function (str, opts) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
+
+var cheerio = require('cheerio');
+
+var Promise = require('pinkie-promise');
+
+var got = require('got');
+
+module.exports = function (module) {
+	if (typeof module !== 'string') {
+		return Promise.reject(new Error('module required'));
 	}
 
-	opts = opts || {};
+	var url = 'https://www.npmjs.com/package/' + module;
 
-	return str + ' & ' + (opts.postfix || 'rainbows');
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+
+		return {
+			lastDay: $('.daily-downloads').text() + ' downloads' || null,
+
+			lastWeek: $('.weekly-downloads').text() + ' downloads' || null,
+
+			lastMonth: $('.monthly-downloads').text() + ' downloads' || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'Sorry! The requested package is not available on NPM';
+		}
+
+		throw err;
+	});
 };
+
